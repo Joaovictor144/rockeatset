c055fe16b515ccd43e8804f5a4d6cfa660673d8d
Joaovictor144 <jvmachado144@gmail.com>
2021-11-09 14:43:32 -0300

    UPDATE REPO

---
+++ chapter-1/src/components/RepositoryList.jsx
@@ -0,0 +1,35 @@
+export function RepositoryList(){
+    return(
+        <section>
+            <h1>Lista de Repositorios</h1>
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>forms in react</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+
+                <li>
+                    <strong>Unform</strong>
+                    <p>forms in react</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+
+                <li>
+                    <strong>Unform</strong>
+                    <p>forms in react</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
