4d1766de3c27423ce882b28d95309b799bfa3772
Joaovictor144 <jvmachado144@gmail.com>
2021-11-17 19:47:01 -0300

    .

--- chapter-1/src/components/RepositoryList.jsx
+++ chapter-1/src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import RepositoryItem from "./RepositoryItem"; 
 import '../styles/repositories.scss';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
 
 //https://api.github.com/orgs/rocketseat/repos
 const repository = {
@@ -9,7 +10,14 @@ const repository = {
     link:"https://github.com/unform/unform",
 }
 export default function RepositoryList(){
-    const [] = useState(() =>{})
+    const [repositories,setRepositories] = useState([]);
+
+    useEffect(()=>{
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+        .then(reponse => reponse.json())
+        .then(data => setRepositories(data))
+    },[])
+
     return(
         <section className='repository-list'>
             <h1>Lista de Repositorios</h1>
